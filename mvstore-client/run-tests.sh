69387aad3dc88e425c26f08fd42cc275460602c8
Max Windisch <mwindisch@videotron.ca>
2011-09-09 13:58:57 -0400

    fix protobuf linking

--- mvstore-client/run-tests.sh
+++ mvstore-client/run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-LD_LIBRARY_PATH=./node_modules/protobuf-for-node:../../protobuf/lib  NODE_PATH=./node_modules/protobuf-for-node node tests.js
+LD_LIBRARY_PATH=./node_modules/protobuf-for-node:../../protobuf/src/.libs  NODE_PATH=./node_modules/protobuf-for-node node tests.js
